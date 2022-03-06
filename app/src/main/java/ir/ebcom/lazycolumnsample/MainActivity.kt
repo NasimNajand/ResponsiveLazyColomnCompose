49e6d08be87b2507e63f4c59867f247a6bf0047f
enolethinq <najandchemiker@gmail.com>
2022-03-06 13:49:43 +0330

    initial commit

---
+++ app/src/main/java/ir/ebcom/lazycolumnsample/MainActivity.kt
@@ -0,0 +1,104 @@
+package ir.ebcom.lazycolumnsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ir.ebcom.lazycolumnsample.ui.theme.LazyColumnSampleTheme
+import ir.ebcom.lazycolumnsample.ui.theme.pastelLemon
+import ir.ebcom.lazycolumnsample.ui.theme.pastelPink
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyColumnSampleTheme {
+                val windowInfo = fetchWindowInfo()
+                if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact){
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth()
+                    ){
+                        items(10){
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(pastelLemon)
+                                    .padding(16.dp)
+                            )
+                        }
+                        items(10){
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(pastelPink)
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
+                }else {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ){
+                            items(10){
+                                Text(
+                                    text = "Item $it",
+                                    fontSize = 52.sp,
+                                    modifier = Modifier.fillMaxWidth()
+                                        .background(pastelLemon)
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ){
+                            items(10){
+                                Text(
+                                    text = "Item $it",
+                                    fontSize = 52.sp,
+                                    modifier = Modifier.fillMaxWidth()
+                                        .background(pastelPink)
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LazyColumnSampleTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/ir/ebcom/lazycolumnsample/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package ir.ebcom.lazycolumnsample.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val pastelLemon = Color(0xFFFFF5BA)
+val pastelPink = Color(0xFFFFCCF9)
